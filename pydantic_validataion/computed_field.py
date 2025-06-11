af383059859558d90147a533dbd320b34d3dd2ca
azher ali <azhermurad@gmail.com>
2025-06-11 12:50:37 +0500

    model dump method

--- pydantic_validataion/computed_field.py
+++ pydantic_validataion/computed_field.py
@@ -2,40 +2,52 @@ from pydantic import (
     BaseModel,
     ValidationError,
     Field,
-    model_validator
+    computed_field
 )
 from typing import List, Optional, Annotated
+
+# nested model in the pydantic 
+class Address(BaseModel):
+    country: str
+    city: str
+    pin: int
 
 
 class User(BaseModel):
     name: str = Field(max_length=24)
     password: str
     password_repeat: str
+    address: Address
     
     
-    # the model validator is validation on the model
-    @model_validator(mode="after")
-    def check_password_match(model):
-        print("sssssss",model.name)
-        if model.password != model.password_repeat:
-            raise ValueError("password and password_repeat should be match!!")
-        return model
-        
+    
+    @computed_field
+    @property
+    def full_name(self)->str:
+        return self.name + " added"
+    
+  
     
     
 def create_user(user:User):
     print(user.name)
     print(user.password)
     print(user.password_repeat)
+    print(user.address.city)
     
 
 try:
+    
+    address = Address(**{"country":"pakistan","city":"islamabad","pin":19182})
     data = {
         "name":"Azher ali",
         "password":"1234",
-        "password_repeat":"1234"
+        "password_repeat":"1234",
+        "address": address
     }
     user = User(**data)
     create_user(user)
+    
+    print(user.model_dump(),"model dump to create user")
 except ValidationError as e:
     print(e.errors())
