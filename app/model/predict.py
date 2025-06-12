88e91e50bb96f0b39315869d868e8c42dca9d125
azher ali <azhermurad@gmail.com>
2025-06-12 18:32:45 +0500

    dockerize the app

--- app/model/predict.py
+++ app/model/predict.py
@@ -1,10 +1,11 @@
 import pickle
 
 # load the model
-with open("model/model.pkl", "rb") as f:
+with open("app/model/model.pkl", "rb") as f:
     model = pickle.load(f)
     
 def load_model(df):
     clicked_on_ad = model.predict(df)[0]
     res = "Clicked" if clicked_on_ad else "Not Clicked"
     return res
+
